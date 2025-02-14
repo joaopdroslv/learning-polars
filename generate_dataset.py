6f7a5e0ab6204e9dab152911718dc680db219382
João Pedro <pdrojoao.slv@gmail.com>
2025-02-13 22:33:19 -0300

    seeder script adjusted & small adjustments to some notebooks

--- generate_dataset.py
+++ generate_dataset.py
@@ -5,44 +5,60 @@ import random
 import datetime
 
 
-fake = Faker()
+fake = Faker('en_US')
 
 
-qtt_products = 50
-products = set()
-while len(products) < qtt_products:
-    products.add(fake.word())
-products = list(products)
+products_amnt = 250
+product_names = set()
 
-qtt_buyers = 100
-buyers = set()
-while len(buyers) < qtt_buyers:
-    buyers.add(f'{fake.first_name()} {fake.last_name()}')
-buyers = list(buyers)
+while len(product_names) < products_amnt:
+    product_names.add(fake.unique.word())
 
-qtt_sellers = 25
-sellers = set()
-while len(sellers) < qtt_sellers:
-    sellers.add(f'{fake.first_name()} {fake.last_name()}')
-sellers = list(sellers)
+products = [(name, round(random.uniform(10, 1000), 2)) for name in product_names]
 
+buyers_amnt = 1000
+buyers = [(f'{fake.first_name()} {fake.last_name()}', fake.phone_number()) 
+          for _ in range(buyers_amnt)]
 
-qtt = 10000000
+sellers_amnt = 50
+sellers = [(f'{fake.first_name()} {fake.last_name()}', fake.phone_number()) 
+           for _ in range(sellers_amnt)]
 
+expected_amnt = 10_000_000
 start_date = datetime.date(2020, 1, 1)
 end_date = datetime.date(2025, 1, 1)
 delta_days = (end_date - start_date).days
 
-data = {
-    "product": [random.choice(products) for _ in range(qtt)],
-    "buyer": [random.choice(buyers) for _ in range(qtt)],
-    "seller": [random.choice(sellers) for _ in range(qtt)],
-    "date": [start_date + datetime.timedelta(days=random.randint(0, delta_days)) for _ in range(qtt)],
-    "sale_price": [round(random.uniform(10, 1000), 2) for _ in range(qtt)],
-    "units_sold": [random.randint(1, 10) for _ in range(qtt)],
-}
+sales_data = []
 
-df = pl.DataFrame(data)
-df.write_csv("sales.csv")
+for _ in range(expected_amnt):
+    product_tuple = random.choice(products)
+    buyer_tuple = random.choice(buyers)
+    seller_tuple = random.choice(sellers)
 
-print("File 'sales.csv' saved!")
+    product = product_tuple[0]
+
+    buyer = buyer_tuple[0]
+    buyer_phone = buyer_tuple[1]
+    shipped_to = fake.address()
+
+    seller = seller_tuple[0]
+    seller_phone = seller_tuple[1]
+
+    date = start_date + datetime.timedelta(days=random.randint(0, delta_days))
+    base_price = product_tuple[1]
+    sale_price = round(base_price * (1 + random.uniform(-0.1, 0.1)), 2)
+    units_sold = random.randint(1, 10)
+
+    sales_data.append(
+        (product, buyer, buyer_phone, shipped_to, seller, seller_phone, date, sale_price, units_sold))
+
+df = pl.DataFrame(
+    sales_data,
+    schema=['product', 'buyer', 'buyer_phone', 'shipped_to', 
+            'seller', 'seller_phone', 'date', 'sale_price', 'units_sold'],
+)
+
+df.write_csv('sales.csv')
+
+print('File "sales.csv" saved!')
